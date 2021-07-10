e174a4d667889ec3fcc10ea8dfe5957dea7d9036
meipolka831 <81464733+meipolka831@users.noreply.github.com>
2021-07-10 11:06:12 +0900

    ログイン条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true
   validates :surname, presence: true
   validates :name, presence: true
   validates :surnamekana, presence: true
