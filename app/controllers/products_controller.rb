2bb08830cfec6338d07bc18bee2f7fd3912e9d7c
meipolka831 <81464733+meipolka831@users.noreply.github.com>
2021-08-03 15:17:58 +0900

    プルリクエストの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @products = Product.all
     @product = Product.order("id DESC")
   end
 
