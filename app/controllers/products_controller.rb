006eac7810b6e2c9c61564864a9e85fcda2a0589
meipolka831 <81464733+meipolka831@users.noreply.github.com>
2021-08-05 18:44:33 +0900

    プルリクエストの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show ,:edit, :update, :destroy]
-  before_action :edit_product, only: [:edit, :update]
+  before_action :edit_product, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.order("id DESC")
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
   root "products#index"
   devise_for :users
-  resources :products, only: [:new,:index,:create,:show, :edit, :update, :destroy]
-
+  resources :products
 end
